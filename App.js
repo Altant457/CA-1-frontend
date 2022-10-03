2d650e44c89b4cbc2b2b719bf0b283fd913c5748
sebastian1234 <slarvad@icloud.com>
2022-10-03 16:26:28 +0200

    before change

--- App.js
+++ App.js
@@ -1,17 +1,23 @@
 function changeText(element) {
-    if (element == 'person') {
+    if (element === 'person') {
         document.getElementById("bar").placeholder = `Skriv fulde navn`
-    } else if (element == 'hobby') {
+    } else if (element === 'hobby') {
         document.getElementById("bar").placeholder = `Skriv hobbyens navn`
-    } else if (element == 'phone') {
+    } else if (element === 'phone') {
         document.getElementById("bar").placeholder = `Skriv telefonnummer`
-    } else if (element == 'address') {
+    } else if (element === 'address') {
         document.getElementById("bar").placeholder = `Skriv adresse`
-    } else if (element == 'zip') {
+    } else if (element === 'zip') {
         document.getElementById("bar").placeholder = `Skriv postnummer`
     }
 }
 
-function test() {
-    console.log("Hello");
+
+
+
+function test(callback) {
+  let element = document.getElementById()
+  const send = callback(element)
+  console.log(send);
 }
+
