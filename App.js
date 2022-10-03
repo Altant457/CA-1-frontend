fb006ad03950d356c0132e9d481e122343f19f27
Marie Fiskbæk <marie.fiskbak@gmail.com>
2022-10-03 16:03:02 +0200

    change placeholder in App.js

--- App.js
+++ App.js
@@ -1,7 +1,22 @@
- function changeText(element) {
-    document.getElementById("bar").placeholder=`Søg ${element}`
-  }
+// function changeText(element) {
+//    document.getElementById("bar").placeholder=`Søg ${element}`
+//  }
 
-  function test () {
+
+function changeText(element) {
+    if (element == 'person') {
+        document.getElementById("bar").placeholder = `Skriv fulde navn`
+    } else if (element == 'hobby') {
+        document.getElementById("bar").placeholder = `Skriv hobbyens navn`
+    } else if (element == 'phone') {
+        document.getElementById("bar").placeholder = `Skriv telefonnummer`
+    } else if (element == 'address') {
+        document.getElementById("bar").placeholder = `Skriv adresse`
+    } else if (element == 'zip') {
+        document.getElementById("bar").placeholder = `Skriv postnummer`
+    }
+}
+
+function test() {
     console.log("Hello");
-  }
+}
